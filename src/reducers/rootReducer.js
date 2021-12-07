53a9a26d1d17944a8534388586f37056ae95e3ef
Jackson Wold <jacksonrwold@gmail.com>
2021-12-07 16:12:39 -0600

    added in the thunk and installed redux-thunk

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,18 @@
 const initialState = {
-    placeholder: 0
+    runs: []
+}
+
+export const getRunData = () => (dispatch, getState) => {
+    fetch("http://localhost:5000/runs").then(response =>
+      response.json().then(responseData => {
+        rootReducer.dispatch({ type:"GET_RUNS", payload: responseData})
+      })
+    )
 }
 
 function rootReducer(state = initialState, action) {
     if (action.type === "PLACEHOLDER") {
-        return {placeholder: state.placeholder}
+        return {runs: state.runs}
     } else {
         return state
     }
